00c4f6eeb56a7cd1473e28091bb2bba0d7bf3a0d
Leifu Chen <leifu.chen@gmail.com>
2023-05-18 02:10:30 +0300

    Update the kwenta logo

--- app/src/page_helpers/common/Layout/LayoutDesktopNav.tsx
+++ app/src/page_helpers/common/Layout/LayoutDesktopNav.tsx
@@ -54,7 +54,7 @@ export default function LayoutDesktopNav(): JSX.Element {
         <Flex width="100%" px={6}>
           <Flex alignItems="center" width={SIDE_WIDTH}>
             <BaseLink display="flex" alignItems="center" href={getPagePath({ page: PageId.TradeIndex })}>
-              <Image src={getAssetSrc('/images/logo.png')} height={32} width={32} />
+              <Image src={getAssetSrc('/images/logo.svg')} height={25} width={157} />
             </BaseLink>
           </Flex>
           <Flex flexGrow={1} alignItems={'center'} justifyContent={'center'}>
